9377d698d921b518ae862dd651be0b700325e240
yyang192 <yiyi.yang3@mail.mcgill.ca>
2022-10-29 08:16:37 -0400

    add comments to code

--- src/traverse_video.py
+++ src/traverse_video.py
@@ -14,6 +14,7 @@ DEVELOPER_KEY = "PUT YOUR DEVELOPER KEY"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
+
 class YoutubeIterator:
     def __init__(self):
         options = webdriver.ChromeOptions()
@@ -30,12 +31,10 @@ class YoutubeIterator:
                                 options=options, version_main=106, use_subprocess=True)
         self.search_start = 'https://www.youtube.com/results?search_query='
 
-    def end_session(self):
-        """Close out browser."""
-        self.driver.quit()
-
     def user_login(self, username, password):
         self.driver.delete_all_cookies()
+
+        # log out first if the previous session was not logged out
         try:
             self.driver.get('https://www.youtube.com/')
             WebDriverWait(self.driver, 60).until(
@@ -45,119 +44,127 @@ class YoutubeIterator:
         except:
             pass
 
+        # go to the log in page for YouTube Google accounts
         self.driver.get('https://accounts.google.com/ServiceLogin?service=youtube&uilel=3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den%26next%3Dhttps%253A%252F%252Fwww.youtube.com%252F&hl=en&ec=65620')
 
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(
-            (By.ID, 'identifierId'))).send_keys(username)
+        # try with directly enter the username
+        try:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(
+                (By.ID, 'identifierId'))).send_keys(username)
+
+        # Google saves other accounts that are previously logged out during the session, so in that case we can't log in directly
+        # so we click to log in with a different account
+        except:
+            WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, 'BHzsHc'))).click()
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(
+                (By.ID, 'identifierId'))).send_keys(username)
 
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(
             (By.XPATH, '//*[@id="identifierNext"]'))).click()
 
-
-        WebDriverWait(self.driver, 5).until(
+        WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, '//input[@type="password"]'))).send_keys(password)
 
-
-        WebDriverWait(self.driver, 5).until(
+        WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, '//*[@id="passwordNext"]/div/button'))).click()
-        time.sleep(5)
 
-    def run(self, username, password,categoryList,length_interval):
-        """Build data frame from list of queries and sanitize results"""
-        self.user_login(username=username, password=password)
+    def watch_from_homepage(self, categoryList, percentage_interval, length_interval):
+        '''
+        Start watching video from YouTube homepage. Will check if the video is in
+        the category that we are interested in and if the duration of the video is within
+        the range that we specified. As soon as we find a video that filfills the criteria,
+        the program will start watching the video and go through the process of clicking sidebar.
+        '''
 
-        self.watch_from_homepage(categoryList,length_interval)
-
-        self.click_sidebar(3,length_interval)
-        print("EXECUTED SUCCESSFULLY")
-        return
-
-    def log_out(self):
         self.driver.get('https://www.youtube.com/')
-        WebDriverWait(self.driver, 60).until(
-            EC.element_to_be_clickable((By.XPATH, '//*[@id="avatar-btn"]'))).click()
-        WebDriverWait(self.driver, 60).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/ytd-app/ytd-popup-container/tp-yt-iron-dropdown/div/ytd-multi-page-menu-renderer/div[3]/div[1]/yt-multi-page-menu-section-renderer[1]/div[2]/ytd-compact-link-renderer[4]/a/tp-yt-paper-item/div[2]/yt-formatted-string[1]'))).click()
 
+        # get all the ids of the video recommended on the home page
+        ids = self.get_ids("id", "video-title-link")
 
-        
-    
-    def watch_from_homepage(self,categoryList,length_interval):
-        self.driver.get('https://www.youtube.com/')
-        time.sleep(3)
-
-        ids = self.getIds("id","video-title-link")
-        
+        # url for visiting a page
         url = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id={id}&key={api_key}"
+
         for id in ids:
-            data = requests.get(url.format(id=id, api_key=DEVELOPER_KEY)).json()
-            
+            # get metadata of a video
+            data = requests.get(url.format(
+                id=id, api_key=DEVELOPER_KEY)).json()
+
+            # sometimes the id doesn't point to a video but a livestream session or a playlist,
+            # in that case 'items' will be empty, so we need to check if it's a valid video
             if data['items']:
                 if int(data['items'][0]['snippet']['categoryId']) in categoryList:
-                    duration = self.getDuration(id)
-                    if duration>0 and duration<=1800:
-                        self.watch_video(id,self.getRandomLengthPercentage(length_interval))
+                    duration = self.get_duration(id)
+                    if duration > length_interval[0] and duration <= length_interval[1]:
+                        # watch the video if passed the checks for a random length within the range
+                        self.watch_video(
+                            id, self.get_random_length_percentage(percentage_interval))
                         break
-                    
 
+    def watch_video(self, id, percentage):
+        '''
+        Watch a video for a certain duration. The duration is obtained by calculating with 
+        the percentage specified since users usually don't watch the video till the very end.
+        Will go back to the program where this function is called after getting to percentage
+        of the total length of the video.
+        '''
+        # get video total length by visiting the url
+        duration = self.get_duration(id)
 
-
-    def watch_video(self,id,percentage):
-        #get video total length by visiting the url
-        duration = self.getDuration(id)
-        
-        #calculate the percentage of the length
+        # calculate the percentage of the length
         watch_time = duration * percentage/100
-        
         self.driver.get('https://www.youtube.com/watch?v='+id)
         time.sleep(2)
 
-        #how to deal with ads??
+        # skip adds when possible
         try:
             WebDriverWait(self.driver, 60).until(
-                EC.element_to_be_clickable(By.CLASS_NAME,'ytp-ad-skip-button-container'))
+                EC.element_to_be_clickable(By.CLASS_NAME, 'ytp-ad-skip-button-container'))
         except:
             pass
-        
-        #return when the time is up
-        time.sleep(watch_time)
-        
-        return
 
-    
-    def click_sidebar(self, number, length_interval):
+        # watch until time is up
+        time.sleep(watch_time)
+
+    def click_sidebar(self, number, percentage_interval, length_interval):
+        '''Watch videos from sidebar until number of videos specified is reached.'''
         watched = 0
-        time.sleep(2)
-        
-        #while hitting a certain number
-        while watched<number:
-            ids = self.getIds(By.CSS_SELECTOR,".yt-simple-endpoint.style-scope.ytd-compact-video-renderer")
-            self.watch_video(self.getRandomChoice(ids),self.getRandomLengthPercentage(length_interval))
-            watched+=1
-        #continue to randomly select video from sidebar
-        return
-    
-    
-    def  getRandomChoice(self,list):
+
+        # keep watching until enough
+        while watched < number:
+            #get a list of the ids of the videos on the sidebar
+            ids = self.get_ids(
+                By.CSS_SELECTOR, ".yt-simple-endpoint.style-scope.ytd-compact-video-renderer")
+            #randomly choose one to watch from teh list
+            self.watch_video(self.get_random_choice(
+                ids, length_interval), self.get_random_length_percentage(percentage_interval))
+            watched += 1
+
+    def get_random_choice(self, list, length_interval):
+        '''Helper function taht choose one video(that filfills the video length criteria) by random from a list of videos.'''
         choice = randrange(len(list))
-        while self.getDuration(list[choice])<=0 or self.getDuration(list[choice])>1800:
+        while self.get_duration(list[choice]) <= length_interval[0] or self.get_duration(list[choice]) > length_interval[1]:
             choice = randrange(len(list))
         return list[choice]
 
-    def getRandomLengthPercentage(self,list):
-        return randrange(list[0],list[1])   
-    
-    def getDuration(self,id):
+    def get_random_length_percentage(self, list):
+        '''
+        Helper function that returns an interger from the interval given. For example, if we want
+        to stop watching a video between 50% to 80% of its total length, this function takes [50,80]
+        and will return an integer between that interval.
+        '''
+        return randrange(list[0], list[1])
+
+    def get_duration(self, id):
         url = 'https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id={id}&key={api_key}'
-        data = requests.get(url.format(id=id,api_key=DEVELOPER_KEY)).json()
-        print(data)
+        data = requests.get(url.format(id=id, api_key=DEVELOPER_KEY)).json()
         seconds = 0
         if data['items']:
             duration = data['items'][0]['contentDetails']['duration']
-            day = re.findall('(\d+)D',duration)
-            hour = re.findall('(\d+)H',duration)
-            minute = re.findall('(\d+)M',duration)
-            second = re.findall('(\d+)S',duration)
+            day = re.findall('(\d+)D', duration)
+            hour = re.findall('(\d+)H', duration)
+            minute = re.findall('(\d+)M', duration)
+            second = re.findall('(\d+)S', duration)
             if day:
                 seconds += int(day[0])*24*60*60
             if hour:
@@ -166,8 +173,6 @@ class YoutubeIterator:
                 seconds += int(minute[0])*60
             if second:
                 seconds += int(second[0])
-            
-            print(seconds)
             return seconds
         return -1
 
@@ -183,17 +188,62 @@ class YoutubeIterator:
             print('CategoryId EMPTY! API Request Failed')
         return short_categoryId
 
-    def getIds(self,by,identifier_name):
-        links = self.driver.find_elements(by,identifier_name)
+    def get_ids(self, by, identifier_name):
+        '''A helper function to collect '''
+        links = self.driver.find_elements(by, identifier_name)
         ids = []
         ids += [link.get_attribute('href') for link in links]
-        ids = [id.strip('https://www.youtube.com/watch?v=') for id in filter(None,ids)]
+        ids = [id.strip('https://www.youtube.com/watch?v=')
+               for id in filter(None, ids)]
         return ids
-    
+
+    def log_out(self):
+        '''Log out of the current account.'''
+        # go to YouTube homepage
+        self.driver.get('https://www.youtube.com/')
+
+        # click on the profile photo in the up right corner
+        WebDriverWait(self.driver, 60).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="avatar-btn"]'))).click()
+
+        # click logout
+        WebDriverWait(self.driver, 60).until(
+            EC.element_to_be_clickable((By.XPATH, '/html/body/ytd-app/ytd-popup-container/tp-yt-iron-dropdown/div/ytd-multi-page-menu-renderer/div[3]/div[1]/yt-multi-page-menu-section-renderer[1]/div[2]/ytd-compact-link-renderer[4]/a/tp-yt-paper-item/div[2]/yt-formatted-string[1]'))).click()
+
+    def end_session(self):
+        '''Terminate the driver.'''
+        self.driver.quit()
+
+    def run(self, usernames, password, categoryList, percentage_interval, length_interval, num_sidebar):
+        '''
+        Main function that orchestrate the whole process of traversing through videos.
+        Iterate through the list of usernames in order and make each one of the accounts to watch videos according to
+        the speficiations.
+        '''
+        for i in range(len(usernames)):
+            self.user_login(username=usernames[i], password=password[i])
+
+            self.watch_from_homepage(
+                categoryList[i], percentage_interval[i], length_interval[i])
+
+            self.click_sidebar(
+                num_sidebar[i], percentage_interval[i], length_interval[i])
+            self.log_out()
+
+        print("EXECUTED SUCCESSFULLY")
+
+
 if __name__ == '__main__':
     iterator_ = YoutubeIterator()
-    username = 'prometheuslabai00001@gmail.com'
-    password = 'nanababa123'
-    categoryList = [24,10]
-    length_interval = [50,80]
-    iterator_.run(username, password,categoryList,length_interval)
+
+    ######################MODIFY HERE TO USE THE PROGRAM#######################
+    usernames = ['USERNAME1', 'USERNAME2']
+    password = ['PASSWORD1', 'PASSWORD2']
+    categoryList = [[24, 10, 22, 27, 23], [24, 23, 28, 20, 10]]
+    percentage_interval = [[50, 80], [50, 80]]
+    length_interval = [[0, 18000], [0, 18000]]
+    num_sidebar = [2, 2]
+    ###########################################################################
+
+    iterator_.run(usernames, password, categoryList,
+                  percentage_interval, length_interval, num_sidebar)
